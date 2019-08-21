5a1ec586cec8bcc2527302b15e5c11633b8a2ede
kmr <ginjirokimura0831@gmail.com>
2019-08-21 02:24:10 +0000

    Add kaminari gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     
     def after_sign_in_path_for(resource)
       # admin_root_path
-      root_path
+      events_path
     end
 
     def after_sign_out_path_for(resource)
@@ -26,12 +26,12 @@ class ApplicationController < ActionController::Base
     
     def after_sign_up_path_for(resource)
       # admin_root_path
-      root_path
+      events_path
     end
 
     def after_inactive_sign_up_path_for(resource)
       # admin_root_path
-      root_path
+      events_path
     end
 
   protected
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,9 @@
 class EventsController < ApplicationController
     before_action :authenticate_user!, except: :show
+    PER = 3
     
     def index
-       @events = Event.where('start_time > ?', Time.zone.now).order(:start_time) 
+       @events = Event.page(params[:page]).per(PER).where('start_time > ?', Time.zone.now).order(:start_time) 
     end
     
     def new
