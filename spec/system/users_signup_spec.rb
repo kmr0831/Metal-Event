a502c44f65828a437fb8125c321bfd5efe099495
kmr <ginjirokimura0831@gmail.com>
2019-08-27 10:50:18 +0000

    Finish users_signin_spec.rb

--- spec/system/users_signup_spec.rb
+++ spec/system/users_signup_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe 'UsersSignup', type: :system do
     expect(page).to have_content 'サインアップ'
   end
   
+  it 'ログインできる' do
+    visit new_user_registration_path
+    fill_in 'user[name]', with: 'Example'
+    fill_in 'user[email]', with: 'example@example.com'
+    fill_in 'user[password]', with: 'password'
+    fill_in 'user[password_confirmation]', with: 'password'
+    expect { click_on '送信'}.to change { User.count }.by(1)
+    expect(page).to have_content 'イベント一覧'
+  end
 end
